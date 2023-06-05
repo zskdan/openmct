689965416ebfeef385218384b25100358a95538d
Zakaria ElQotbi <zakaria@elqotbi.com>
2023-06-06 01:00:50 +0200

    align with leafletmap

---
+++ example/mapVuePlugin/LeafletMapView.js
@@ -0,0 +1,37 @@
+define([
+    './LeafletMap.vue',
+    'vue'
+], function (
+    LeafletMap,
+    Vue
+) {
+    Vue = Vue.default || Vue;
+    let component;
+    function LeafletMapView(domainObject, openmct, document) {
+	this.domainObject = domainObject;
+	this.openmct = openmct;
+        this.objectAPI = openmct.objects;
+	this.document = document;
+    }
+
+    LeafletMapView.prototype.show = function (domainObject) {
+    	console.log("ZSK:show");
+	component = new Vue({
+	    el: domainObject,
+	    components: {
+		LeafletMap
+	    },
+	    provide: {
+		openmct,
+		domainObject,
+		composition: openmct.composition.get(domainObject)
+	    }
+	});
+    };
+
+    LeafletMapView.prototype.destroy = function () {
+	component.$destroy();
+	component = undefined;
+    };
+    return LeafletMapView;
+});
